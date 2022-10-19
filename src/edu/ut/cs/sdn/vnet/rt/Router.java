07b68d63277ced5590fa3b1f80161ae733654831
Sindhu Yepuri <sindhu@Sindhus-MacBook-Pro-2.local>
2022-10-19 18:32:31 -0500

    check to see if receiving

--- src/edu/ut/cs/sdn/vnet/rt/Router.java
+++ src/edu/ut/cs/sdn/vnet/rt/Router.java
@@ -299,7 +299,8 @@ public class Router extends Device
             IPv4 ip = (IPv4) etherPacket.getPayload();
             UDP udp = (UDP) ip.getPayload();
             RIPv2 rip = (RIPv2) udp.getPayload();
-            this.handleRipPacket(rip, inIface);
+            // this.handleRipPacket(rip, inIface);
+            LOG("received rip packet");
             return;
         }
 
